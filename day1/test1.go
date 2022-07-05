00592178e3774a203af7e5d3063b8058fca907f1
unknown <F1241138@dpbg.lh.com>
2022-07-05 16:53:46 +0800

    update

--- day1/test1.go
+++ day1/test1.go
@@ -6,6 +6,6 @@ import (
 
 func Test1() {
 	var i1 int64 = 9999999
-	var i2 int8 = int32(i1)
+	var i2 int8 = 99
 	fmt.Println(i1, i2)
 }
--- main.go
+++ main.go
@@ -4,10 +4,31 @@ package main
 import (
 	"fmt"
 	"go_day/day1"
+	"go_day/learn"
+	"strconv"
 )
 
 func main() {
 	fmt.Println("Hello World!")
-	day1.Test1()
+	// day1.Test1()
 	day1.Test2()
+
+	//print(len("abcdefghijk"))
+	//learn.Recursion()
+	learn.Mutex1()
+	//str := "hello\n12"
+	str := `hello\n12` + "asd"
+	str1 := []byte(str)
+	str1[0] = 'x'
+	str = string(str1)
+	fmt.Println(str)
+	varsa := 12.6
+	varsa2 := int(varsa)
+	aa := fmt.Sprintf("%f", varsa)
+	a, c := strconv.ParseFloat(aa, 32)
+	fmt.Println(a)
+	fmt.Println(c)
+	fmt.Println(varsa2)
+	learn.Abspath()
+	// learn.Day2()
 }
